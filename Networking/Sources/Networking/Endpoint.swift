bc343bded5d3ef56030c843b1543b141dded342e
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-16 14:43:46 -0300

    Defined endpoint structure

--- Networking/Sources/Networking/Endpoint.swift
+++ Networking/Sources/Networking/Endpoint.swift
@@ -1,8 +1,30 @@
 //
-//  File.swift
+//  EndPoint.swift
 //  
 //
 //  Created by Luis Gustavo on 16/03/23.
 //
 
 import Foundation
+
+public protocol EndPoint {
+    var url: URL? { get }
+    var method: HTTPMethod { get }
+    var headers: [String : String] { get }
+    var body: Data? { get }
+    
+    func createRequest() -> URLRequest?
+}
+
+public extension EndPoint {
+    func createRequest() -> URLRequest? {
+        guard let url = url else { return nil }
+        guard let urlWithQueryParamaters = url.with(queryParameters: [:]) else { return nil }
+        
+        var request = URLRequest(url: urlWithQueryParamaters)
+        request.allHTTPHeaderFields = headers
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        return request
+    }
+}
