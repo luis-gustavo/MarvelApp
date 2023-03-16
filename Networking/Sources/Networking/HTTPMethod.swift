c0ad45b5c363d1ca15129f46706d40da299900c0
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-16 14:43:22 -0300

    Added http methods

--- Networking/Sources/Networking/HTTPMethod.swift
+++ Networking/Sources/Networking/HTTPMethod.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
+//  HTTPMethod.swift
 //  
 //
 //  Created by Luis Gustavo on 16/03/23.
 //
 
 import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
