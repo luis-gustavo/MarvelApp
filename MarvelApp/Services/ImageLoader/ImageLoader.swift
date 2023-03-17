548ac88d033ea7681df0f2268ae63f91b81d40b3
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-17 10:16:58 -0300

    Created image loader with caching

--- MarvelApp/Services/ImageLoader/ImageLoader.swift
+++ MarvelApp/Services/ImageLoader/ImageLoader.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+final class ImageLoader: ImageLoaderProtocol {
+
+    // MARK: - Properties
+    static let shared = ImageLoader()
+    private var imageCache = NSCache<NSString, NSData>()
+
+    // MARK: - Init
+    private init() { }
+
+    // MARK: - ImageLoaderProtocol
+    func downloadImage(_ url: URL, _ completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageCache.object(forKey: key) {
+            completion(.success(data as Data))
+            return
+        }
+
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            let key = url.absoluteString as NSString
+            let value = data as NSData
+            self?.imageCache.setObject(value, forKey: key)
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
