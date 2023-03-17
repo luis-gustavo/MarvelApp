5b12472662eb435c075aa115f7d9478dbd41d23e
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-17 10:11:28 -0300

    Created main coordinator of the app

--- MarvelApp/Coordinator/MainCoordinator.swift
+++ MarvelApp/Coordinator/MainCoordinator.swift
@@ -5,4 +5,20 @@
 //  Created by Luis Gustavo on 16/03/23.
 //
 
-import Foundation
+import UIKit
+
+final class MainCoordinator: Coordinatable {
+
+    // MARK: - Properties
+    private let navigationController: UINavigationController
+
+    // MARK: - Init
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    // MARK: - Coordinatable
+    func start() {
+        navigationController.setViewControllers([TabBarController()], animated: true)
+    }
+}
