76cf1e480051ff076a072ab533c783bf856f7e1a
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-17 10:15:25 -0300

    Created comic list view model and provider

--- MarvelApp/Screens/ComicsList/ComicListViewModel.swift
+++ MarvelApp/Screens/ComicsList/ComicListViewModel.swift
@@ -5,26 +5,96 @@
 //  Created by Luis Gustavo on 16/03/23.
 //
 
+import Foundation
 import Networking
 import Service
 
-final class CharacterListViewModel: CharacterListViewModelProtocol {
+protocol ComicListViewModelDelegate: AnyObject {
+    func loadedInitialComics()
+    func loadedMoreComics(withOriginal originalCount: Int, andNewCount newCount: Int)
+}
 
-    private let networkService: URLSessionNetworkingProtocol = Services.make(for: URLSessionNetworkingProtocol.self)
+final class ComicListViewModel {
+
+    // MARK: - Properties
+    weak var delegate: ComicListViewModelDelegate?
+    var showLoadMore: Bool {
+        offset + count < total
+    }
+    private var nextOffset: Int {
+        showLoadMore ? offset + limit : offset
+    }
+    private(set) var isLoadingMore = false
+    private let comicProvider = ComicProvider()
+    private let limit = 20
+    private var count = 0
+    private var offset = 0
+    private var total = 0
+    private var comics = Set<Comic>() {
+        didSet {
+            for item in comics {
+                let viewModel = ComicCollectionViewCellViewModel(
+                    title: item.title,
+                    id: item.id,
+                    imageUrl: item.thumbnail.url
+                )
+                if !cellViewModels.contains(viewModel) {
+                    cellViewModels.append(viewModel)
+                }
+            }
+        }
+    }
+    private(set) var cellViewModels: [ComicCollectionViewCellViewModel] = []
 
     // MARK: - CharacterListViewModelProtocol
-    func getCharacters() {
-        networkService.request(endPoint: CharacterEndpoint.characters) { result in
+    func fetchComics() {
+        comicProvider.fetchComics(offset: nextOffset, limit: limit) { [weak self] result in
             switch result {
             case let .success(success):
-                print(String(data: success.data, encoding: .utf8))
+                for item in success.data.results {
+                    self?.comics.insert(item)
+                }
+                self?.count = success.data.count
+                self?.offset = success.data.offset
+                self?.total = success.data.total
+                DispatchQueue.main.async {
+                    self?.delegate?.loadedInitialComics()
+                }
             case let .failure(failure):
                 print(failure.localizedDescription)
             }
         }
     }
 
-    func numberOfCharacters() -> Int {
-        return 0
+    func fetchAdditionalComics() {
+        guard !isLoadingMore else { return }
+        isLoadingMore = true
+        comicProvider.fetchComics(offset: nextOffset, limit: limit) { [weak self] result in
+            switch result {
+            case let .success(success):
+                let originalCount = self?.comics.count ?? 0
+                for item in success.data.results {
+                    self?.comics.insert(item)
+                }
+                let newCount = (self?.comics.count ?? 0) - originalCount
+                self?.count = success.data.count
+                self?.offset = success.data.offset
+                self?.total = success.data.total
+                DispatchQueue.main.async {
+                    self?.delegate?.loadedMoreComics(withOriginal: originalCount, andNewCount: newCount)
+                    self?.isLoadingMore = false
+                }
+            case let .failure(failure):
+                print(failure.localizedDescription)
+                self?.isLoadingMore = false
+            }
+        }
+    }
+}
+
+extension Sequence where Element: Hashable {
+    func unique() -> [Element] {
+        var set = Set<Element>()
+        return filter { set.insert($0).inserted }
     }
 }
