d0211888e74240496bed8ea1bdecd5e862f78805
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-17 10:16:08 -0300

    Created loading footer

--- MarvelApp/Screens/ComicsList/FooterLoadingCollectionView.swift
+++ MarvelApp/Screens/ComicsList/FooterLoadingCollectionView.swift
@@ -5,4 +5,46 @@
 //  Created by Luis Gustavo on 17/03/23.
 //
 
-import Foundation
+import UIKit
+
+final class FooterLoadingCollectionReusableView: UICollectionReusableView {
+
+    // MARK: - Properties
+    private let spinner: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(style: .large)
+        view.hidesWhenStopped = true
+        view.startAnimating()
+        return view
+    }()
+
+    // MARK: - Inits
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViewConfiguration()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+// MARK: - ViewCodable
+extension FooterLoadingCollectionReusableView: ViewCodable {
+    func buildViewHierarchy() {
+        addSubviews(
+            spinner
+        )
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
+
+    func setupAdditionalConfiguration() {
+        backgroundColor = .systemBackground
+    }
+}
