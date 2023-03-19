e9bca16b303245d45b3164744c9f1d79aa002976
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-18 21:59:09 -0300

    Created storage package

--- Storage/SQLiteStorage.swift
+++ Storage/SQLiteStorage.swift
@@ -5,4 +5,141 @@
 //  Created by Luis Gustavo on 18/03/23.
 //
 
-import Foundation
+import SQLite
+
+public final class SQLiteStorage: Storage {
+
+    // MARK: - Properties
+    private let connection: Swift.Result<Connection, StorageError>
+
+    // MARK: - Inits
+    public init(_ location: Connection.Location) {
+        let connectionResult = createConnection(location)
+        switch connectionResult {
+        case let .success(connection):
+            let schemaResult = createSchema(at: connection)
+            switch schemaResult {
+            case let .success(connection):
+                self.connection = .success(connection)
+            case let .failure(storageError):
+                self.connection = .failure(storageError)
+            }
+        case let .failure(storageError):
+            self.connection = .failure(storageError)
+        }
+    }
+
+    // MARK: - Storage
+    public func write(ids: [Int], on table: StorageTable) -> Swift.Result<[Int], StorageError> {
+        switch connection {
+        case let .success(db):
+            do {
+                try ids.forEach { id in
+                    switch table {
+                    case .cart:
+                        try db.run(cartTable.insert(
+                            or: .replace,
+                            comicIdExpression <- id,
+                            updatedAtExpression <- Date()
+                        ))
+                    case .favorite:
+                        try db.run(favoritesTable.insert(
+                            or: .replace,
+                            comicIdExpression <- id,
+                            updatedAtExpression <- Date()
+                        ))
+                    }
+                }
+                return .success(ids)
+            } catch {
+                return .failure(.unknown(error))
+            }
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+
+    public func delete(id: Int, on table: StorageTable) -> Swift.Result<Int, StorageError> {
+        switch connection {
+        case let .success(db):
+            do {
+                switch table {
+                case .cart:
+                    let current = cartTable.filter(comicIdExpression == id)
+                    try db.run(current.delete())
+                case .favorite:
+                    let current = favoritesTable.filter(comicIdExpression == id)
+                    try db.run(current.delete())
+                }
+                return .success(id)
+            } catch {
+                return .failure(.unknown(error))
+            }
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+
+    public func read(from table: StorageTable) -> Swift.Result<[Row<Int>], StorageError> {
+        switch connection {
+        case let .success(db):
+            switch table {
+            case .cart:
+                do {
+                    let rows =  try db.prepare(cartTable).map { row -> Row<Int> in
+                        let result = Row<Int>(model: row[comicIdExpression], updatedAt: row[updatedAtExpression])
+                        return result
+                    }
+                    return .success(rows)
+                } catch {
+                    return .failure(.unknown(error))
+                }
+            case .favorite:
+                do {
+                    let rows =  try db.prepare(favoritesTable).map { row -> Row<Int> in
+                        let result = Row<Int>(model: row[comicIdExpression], updatedAt: row[updatedAtExpression])
+                        return result
+                    }
+                    return .success(rows)
+                } catch {
+                    return .failure(.unknown(error))
+                }
+            }
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+}
+
+fileprivate let favoritesTable = Table(StorageTable.favorite.rawValue)
+fileprivate let cartTable = Table(StorageTable.cart.rawValue)
+fileprivate let comicIdExpression = Expression<Int>("comic_id")
+fileprivate let updatedAtExpression = Expression<Date>("updated_at")
+
+fileprivate func createSchema(at connection: Connection) -> Swift.Result<Connection, StorageError> {
+
+    do {
+        try connection.run(favoritesTable.create(ifNotExists: true) { builder in
+            builder.column(comicIdExpression, primaryKey: true)
+            builder.column(updatedAtExpression)
+        })
+
+        try connection.run(cartTable.create(ifNotExists: true) { builder in
+            builder.column(comicIdExpression, primaryKey: true)
+            builder.column(updatedAtExpression)
+        })
+    } catch {
+        return .failure(StorageError.tableCreation(error))
+    }
+
+    return .success(connection)
+}
+
+fileprivate func createConnection(_ location: Connection.Location) -> Swift.Result<Connection, StorageError> {
+    do {
+        let connection = try Connection(location)
+        return .success(connection)
+    } catch {
+        return .failure(StorageError.connectionCreation(error))
+    }
+}
--- Storage/Storage.swift
+++ Storage/Storage.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+public enum StorageTable: String {
+    case favorite, cart
+}
+
+public protocol Storage {
+    func write(ids: [Int], on table: StorageTable) -> Swift.Result<[Int], StorageError>
+    func read(from table: StorageTable) -> Swift.Result<[Row<Int>], StorageError>
+    func delete(id: Int, on table: StorageTable) -> Swift.Result<Int, StorageError>
+}
+
+public struct Row<Model> {
+    public let model: Model
+    public let updatedAt: Date
+
+    public init(model: Model, updatedAt: Date) {
+        self.model = model
+        self.updatedAt = updatedAt
+    }
+}
