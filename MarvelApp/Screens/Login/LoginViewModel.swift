77ac8814debe72f9940d2890a1ed9b986ffe7218
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-19 14:24:04 -0300

    Created login view model

--- MarvelApp/Screens/Login/LoginViewModel.swift
+++ MarvelApp/Screens/Login/LoginViewModel.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+import Service
+
+protocol LoginViewModelDelegate: AnyObject {
+    func updatedButtonIsEnabled(_ isEnabled: Bool)
+    func changedState(_ state: LoginViewModel.State)
+}
+
+final class LoginViewModel {
+
+    // MARK: - State
+    enum State {
+        case idle, fetching
+    }
+
+    // MARK: - Properties
+    weak var delegate: LoginViewModelDelegate?
+    var isButtonEnabled: Bool = false {
+        didSet {
+            if oldValue != isButtonEnabled {
+                delegate?.updatedButtonIsEnabled(isButtonEnabled)
+            }
+        }
+    }
+    private var email: String = "" {
+        didSet {
+            isButtonEnabled = email.isValidEmail && !password.isEmpty
+        }
+    }
+    private var password: String = "" {
+        didSet {
+            isButtonEnabled = email.isValidEmail && !password.isEmpty
+        }
+    }
+    private let loginProvider: LoginProviderProtocol = Services.make(for: LoginProviderProtocol.self)
+    private let router: LoginRouterProtocol
+    private var state: State = .idle {
+        didSet {
+            delegate?.changedState(state)
+        }
+    }
+
+    // MARK: - Init
+    init(router: LoginRouterProtocol) {
+        self.router = router
+    }
+}
+
+// MARK: - Internal methods
+extension LoginViewModel {
+    func updateEmail(_ email: String) {
+        self.email = email
+    }
+
+    func updatePassword(_ password: String) {
+        self.password = password
+    }
+
+    func login() {
+        state = .fetching
+        loginProvider.login(with: email, and: password) { [weak self] result in
+            switch result {
+            case .success:
+                self?.router.showMainView()
+            case let .failure(error):
+                self?.state = .idle
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
