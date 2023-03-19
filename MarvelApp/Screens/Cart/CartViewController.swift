58cbd7fa9708312c5c0283ae5066ee9b02c2614a
Luis Gustavo <luis.gustavo3013@hotmail.com>
2023-03-18 21:59:59 -0300

    Added cart view

--- MarvelApp/Screens/Cart/CartViewController.swift
+++ MarvelApp/Screens/Cart/CartViewController.swift
@@ -5,4 +5,45 @@
 //  Created by Luis Gustavo on 18/03/23.
 //
 
-import Foundation
+import UIKit
+
+final class CartViewController: UIViewController {
+
+    // MARK: - Properties
+    private let viewModel: CartViewModel
+
+    // MARK: - UI Properties
+    private lazy var cartView: CartView = {
+        let view = CartView(viewModel: viewModel)
+        view.delegate = self
+        return view
+    }()
+
+    // MARK: - Inits
+    init(viewModel: CartViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - loadView
+    override func loadView() {
+        view = cartView
+        title = TabBarPage.cart.title
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.fetchComicsFromCart()
+    }
+}
+
+// MARK: - CartViewDelegate
+extension CartViewController: CartViewDelegate {
+    func didSelectComic(at position: Int) {
+        viewModel.showComicDetail(at: position)
+    }
+}
